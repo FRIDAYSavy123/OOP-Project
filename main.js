bd018e1f9a0d47140f327561db4c1395a49cfa08
Abdul Raffay <159190889+FRIDAYSavy123@users.noreply.github.com>
2024-08-03 05:20:37 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+// Create class { class is a group of objects }
+class Person {
+    _personality;
+    constructor() {
+        this._personality = 'Mystery';
+    }
+    // User input
+    userInput(input) {
+        // if-else statements
+        if (input.includes('talk to others about myself')) {
+            this._personality = 'happily';
+        }
+        else if (input.includes('keep quiet and don’t ask questions')) {
+            this._personality = 'talk rudely';
+        }
+    }
+    // Return function
+    getPersonality() {
+        return this._personality;
+    }
+}
+// Create main class
+class Main {
+    async main() {
+        // Await function call
+        const answer = await inquirer.prompt({
+            // Object create
+            type: 'list',
+            name: 'choice',
+            message: chalk.yellow('Tell your behaviour?'),
+            choices: [
+                '1. talk to others about myself',
+                '2. keep quiet and don’t ask questions',
+            ],
+        });
+        // Create a person class instance
+        let myPerson = new Person();
+        myPerson.userInput(answer.choice);
+        // Output response
+        console.log(`You are ${myPerson.getPersonality()}`);
+    }
+}
+// Create and run the Main class instance
+const myObject = new Main();
+myObject.main();
